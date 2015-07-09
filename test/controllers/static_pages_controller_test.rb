b2153b26d75cfba85effa8f4e465ac0bc92c51d6
cynosures <cynosurech@gmail.com>
2015-07-10 02:01:36 +0800

    finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Kuruman"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Kuruman"
   end
 
   test "should get about" do
-    get :about
+    get :about 
     assert_response :success
+    assert_select "title", "About | Kuruman"
   end
 
 end
