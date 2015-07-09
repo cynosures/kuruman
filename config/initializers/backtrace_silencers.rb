3732d6d49b0b8d6423b8e0b75ae23864abde55a3
cynosures <cynosurech@gmail.com>
2015-07-10 02:32:45 +0800

    apply Guard and MiniTest

--- config/initializers/backtrace_silencers.rb
+++ config/initializers/backtrace_silencers.rb
@@ -2,6 +2,8 @@
 
 # You can add backtrace silencers for libraries that you're using but don't wish to see in your backtraces.
 # Rails.backtrace_cleaner.add_silencer { |line| line =~ /my_noisy_library/ }
+# 添加调用跟踪静默程序，过滤 RVM 相关的内容
+Rails.backtrace_cleaner.add_silencer { |line| line =~ /rvm/ }
 
 # You can also remove all the silencers if you're trying to debug a problem that might stem from framework code.
 # Rails.backtrace_cleaner.remove_silencers!
